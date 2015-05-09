37efbab7645d60ad6d93fbc070a7b08ac639638f
koyachi <rtk2106@gmail.com>
2015-05-10 06:52:20 +0900

    refactoring

--- src/Identicon.elm
+++ src/Identicon.elm
@@ -70,38 +70,41 @@ renderIdenticon : Int -> Int -> C.Form
 renderIdenticon code size =
   let patchSize = (size |> toFloat) / 3
       shiftRightAnd shiftBit andBit = (Bitwise.shiftRight code shiftBit) |> (Bitwise.and andBit)
-      isShiftRightAndNotZero shiftBit andBit = ((Bitwise.shiftRight code shiftBit) |> (Bitwise.and andBit)) /= 0
+      isShiftRightAndNotZero shiftBit andBit = (shiftRightAnd shiftBit andBit) /= 0
 
       middleType = Maybe.withDefault 0 (centerPatchTypes |> Array.get (Bitwise.and code 3))
       middleInvert = isShiftRightAndNotZero 2 1
+
       cornerType = shiftRightAnd 3 15
       cornerInvert = isShiftRightAndNotZero 7 1
       cornerTurn = shiftRightAnd 8 3
+
       sideType = shiftRightAnd 10 15
       sideInvert = isShiftRightAndNotZero 14 1
       sideTurn = shiftRightAnd 15 3
+
       blue = shiftRightAnd 16 31
       green = shiftRightAnd 21 31
       red = shiftRightAnd 27 31
       foreColor = Color.rgba (Bitwise.shiftLeft red 3) (Bitwise.shiftLeft green 3) (Bitwise.shiftLeft blue 3) 1.0
       backColor = Color.rgba 0xff 0xff 0xff 1.0
 
-      renderIdenticonPatch' x y size patch turn invert = renderIdenticonPatch x y size patch turn invert foreColor backColor
+      renderIdenticonPatch' x y patch turn invert = renderIdenticonPatch x y patchSize patch turn invert foreColor backColor
   in
     List.concat [
            -- middle patch
-           renderIdenticonPatch' patchSize patchSize patchSize middleType 0 middleInvert,
+           renderIdenticonPatch' patchSize patchSize middleType 0 middleInvert,
 
            -- side patches, starting from top and moving clock-wise
-           renderIdenticonPatch' patchSize 0 patchSize sideType (sideTurn + 0) sideInvert,
-           renderIdenticonPatch' (patchSize*2) patchSize patchSize sideType (sideTurn + 1) sideInvert,
-           renderIdenticonPatch' patchSize (patchSize*2) patchSize sideType (sideTurn + 2) sideInvert,
-           renderIdenticonPatch' 0 patchSize patchSize sideType (sideTurn + 3) sideInvert,
+           renderIdenticonPatch' patchSize 0 sideType (sideTurn + 0) sideInvert,
+           renderIdenticonPatch' (patchSize*2) patchSize sideType (sideTurn + 1) sideInvert,
+           renderIdenticonPatch' patchSize (patchSize*2) sideType (sideTurn + 2) sideInvert,
+           renderIdenticonPatch' 0 patchSize sideType (sideTurn + 3) sideInvert,
            -- corner paths, starting from top left and moving clock-wise
-           renderIdenticonPatch' 0 0 patchSize cornerType (cornerTurn + 0) cornerInvert,
-           renderIdenticonPatch' (patchSize*2) 0 patchSize cornerType (cornerTurn + 1) cornerInvert,
-           renderIdenticonPatch' (patchSize*2) (patchSize*2) patchSize cornerType (cornerTurn + 2) cornerInvert,
-           renderIdenticonPatch' 0 (patchSize*2) patchSize cornerType (cornerTurn + 3) cornerInvert
+           renderIdenticonPatch' 0 0 cornerType (cornerTurn + 0) cornerInvert,
+           renderIdenticonPatch' (patchSize*2) 0 cornerType (cornerTurn + 1) cornerInvert,
+           renderIdenticonPatch' (patchSize*2) (patchSize*2) cornerType (cornerTurn + 2) cornerInvert,
+           renderIdenticonPatch' 0 (patchSize*2) cornerType (cornerTurn + 3) cornerInvert
           ]
       -- centering vertically fliped forms
       |> List.map (\f -> f |> C.move (-patchSize, patchSize))
